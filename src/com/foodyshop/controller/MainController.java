ce44e6e4e544d31808386f231829650772f1b87a
son1233 <son15052002@gmail.com>
2021-11-27 03:07:31 +0700

    GridPane function delete row

--- src/com/foodyshop/controller/MainController.java
+++ src/com/foodyshop/controller/MainController.java
@@ -7,10 +7,13 @@ package com.foodyshop.controller;
 
 import com.foodyshop.main.Navigator;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -24,7 +27,7 @@ public class MainController implements Initializable {
 
     private static final double NAV_DEF_OPACITY = 0.65;
     private static final double NAV_ACTIVE_OPACITY = 1;
-    
+
     @FXML
     private BorderPane rightLayout;
 
@@ -40,13 +43,13 @@ public class MainController implements Initializable {
         // init currentPage item
         currentPage = (HBox) navBar.getChildren().get(1);
         currentPage.setCursor(Cursor.DEFAULT);
-            
-        // init nav event
+//        deleteRow(navBar, 9);
+//        deleteRow(navBar, 9);
         navBar.getChildren().forEach(node -> {
-            if(node != navBar.getChildren().get(0)){
+            if (node != navBar.getChildren().get(0)) {
                 HBox item = ((HBox) node);
                 // set default opacity
-                if(node != navBar.getChildren().get(1)){
+                if (node != navBar.getChildren().get(1)) {
                     node.setOpacity(NAV_DEF_OPACITY);
                 }
                 // set onclick
@@ -72,13 +75,34 @@ public class MainController implements Initializable {
             }
         });
     }
-    
-    private void activateNavItem(HBox item){
+
+    private void deleteRow(GridPane grid, final int row) {
+        Set<Node> deleteNodes = new HashSet<>();
+        for (Node child : grid.getChildren()) {
+            // get index from child
+            Integer rowIndex = GridPane.getRowIndex(child);
+
+            // handle null values for index=0
+            int r = rowIndex == null ? 0 : rowIndex;
+
+            if (r > row) {
+                // decrement rows for rows after the deleted row
+                GridPane.setRowIndex(child, r - 1);
+            } else if (r == row) {
+                // collect matching rows for deletion
+                deleteNodes.add(child);
+            }
+        }
+        // remove nodes from row
+        grid.getChildren().removeAll(deleteNodes);
+    }
+
+    private void activateNavItem(HBox item) {
         item.setStyle("-fx-background-color: linear-gradient(#4568DC, #B06AB3); -fx-border-color: transparent transparent #fff transparent");
         item.setOpacity(NAV_ACTIVE_OPACITY);
     }
-    
-    private void inactivateNavItem(HBox item){
+
+    private void inactivateNavItem(HBox item) {
         item.setStyle("-fx-background-color: transparent; -fx-border-color: transparent transparent #fff transparent");
         item.setOpacity(NAV_DEF_OPACITY);
     }
